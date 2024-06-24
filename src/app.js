6090c83da7d457649f002a300b1d75757480173a
Joanzam <joanzam.com@gmail.com>
2024-06-24 17:35:24 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  let who = [
+    "Charles Darwin",
+    "Aristóteles",
+    "Napoleón Bonaparte",
+    "Julio César"
+  ];
+  let action = ["caminaba", "comia", "pintaba", "dormia"];
+  let what = ["en mi silla", "en mi coche", "con el móvil", "en mi mesa"];
+  let when = [
+    "cuando estaba en la montaña",
+    "mientras estaba en mi casa",
+    "cuando caminaba por la calle",
+    "mientras dormia en el campo"
+  ];
+
+  const generateExcuse = () => {
+    let decimalRandom = Math.random();
+    let indiceWho = Math.floor(decimalRandom * who.length);
+    let indiceAction = Math.floor(decimalRandom * action.length);
+    let indiceWhat = Math.floor(decimalRandom * what.length);
+    let indiceWhen = Math.floor(decimalRandom * when.length);
+    let wordWho = who[indiceWho];
+    let wordAction = action[indiceAction];
+    let wordWhat = what[indiceWhat];
+    let wordWhen = when[indiceWhen];
+
+    let excuse = wordWho + " " + wordAction + " " + wordWhat + " " + wordWhen;
+    return excuse;
+  };
+
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
